2bf987245ae3b88c95576805328826d9ea2b578d
BuildTools <unconfigured@null.spigotmc.org>
2020-08-04 11:44:53 +0200

    Update git_clone.js

--- commands/Developer/git_clone.js
+++ commands/Developer/git_clone.js
@@ -3,24 +3,31 @@ var { exec } = require('child_process');
 const { colors, confirmAction, newEmb } = require('../utilities');
 
 module.exports = {
-    name: 'Shutdown',
-    syntax: 'shutdown',
-    args: false,
-    description: 'Stops the Bot',
-    perm: 'ADMINISTRATOR',
-    commands: ['shutdown','restart', 'disconnect'],
-    
-    /**
-     *@document
-     * @this
-     * @param {Message} msg Nachricht in dem der Befehl geschickt wurde
-     * @param {String[]} args Argumente die im Befehl mitgeliefert wurden
-     */
-    execute(msg, args) {
-        confirmAction(msg, 'Willst du den Bot wirklich herunterfahren?', async () => {
-          process.exit();
-        }, () => {
-          
-        })
-    }
+  name: 'Shutdown',
+  syntax: 'shutdown',
+  args: false,
+  description: 'Stops the Bot',
+  perm: 'ADMINISTRATOR',
+  commands: ['shutdown', 'restart', 'disconnect'],
+
+  /**
+   *@document
+   * @this
+   * @param {Message} msg Nachricht in dem der Befehl geschickt wurde
+   * @param {String[]} args Argumente die im Befehl mitgeliefert wurden
+   */
+  execute(msg, args) {
+    confirmAction(msg, 'Willst du den Bot wirklich updaten via Git?', async () => {
+      exec("ls", function (error, stdout, stderr) {
+        msg.channel.send(newEmb(msg)
+        .setTitle("Cloned Git, Results")
+        .addField("**Error:**", `\`${error.message}\``)
+        .addField("**Stdout:**", stdout)
+        .addField("**Stderr:**", stderr)
+        )
+      });
+    }, () => {
+
+    })
+  }
 };

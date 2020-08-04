5698139dc033d1e6b06f9fd961108929194336c1
Maf <dragoncatm@gmail.com>
2020-08-04 13:38:47 +0200

    Update serverinfo.js

--- commands/Info/serverinfo.js
+++ commands/Info/serverinfo.js
@@ -67,7 +67,7 @@ module.exports = {
         roles_s =
           roles
             //.map(r => (r.mentionable ? `<@&${r.id}>` : "`" + r.name + "`"))
-            .map(r => r.toString())
+            .map(r => "<@&"+r.id+">")
             .join(" | ") + " **. . .**";
 
       } else {

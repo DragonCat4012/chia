84d97afe642a9e46787402ddc7a2cde8f23c3fdf
BuildTools <unconfigured@null.spigotmc.org>
2020-08-04 12:08:53 +0200

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -10,8 +10,8 @@ module.exports = {
     name: 'kick',
     syntax: 'kick <@user> [@user]...',
     args: true,
-    description: 'Entfernt den unwürdige Wesen von deinem Server ^^',
- perm: 'KICK_MEMBERS',
+    description: 'Entfernt die unwÃ¼rdigen Wesen von deinem Server ^^',
+    perm: 'KICK_MEMBERS',
     commands: ['kick'],
 
     /**
@@ -21,35 +21,35 @@ module.exports = {
      * @param {String[]} args Argumente die im Befehl mitgeliefert wurden
      */
     async execute(msg) {
-        let emb = newEmb(msg)
-            
+        let emb = newEmb(msg).setColor(colors.nothing);
+
         let members = msg.mentions.members.array();
         let not_kicked = new Array();
         let kicked = new Array();
         let reason = 'Kicked by ' + msg.author.tag;
-       
+
         for (let member of members) {
             try {
-                kicked.push(member).then
-                await member.kick(reason) ;
+                await member.kick(reason);
+                kicked.push(member);
             } catch (err) {
                 console.log(err)
-                msg.channel.send( `**Fehler bei ${member}**` + err )
+                msg.channel.send(`**Fehler bei ${member}**` + err)
                 not_kicked.push(member);
             }
         }
-        let kickedamount = members.length - not_kicked.length;
-        
-       if(not_kicked.length > 0) {
-           emb.setDescription(not_kicked.join(', '))
-           msg.channel.send(emb.setTitle(`Diese(n) ${not_kicked.length} Nutzer konnte ich leider nicht kicken qwq`).setFooter(`${kickedamount} Nutzer wurden gekickt`).setColor(colors.nothing))}
 
-       if (kicked.length > 0) {
-        emb.setDescription(kicked.join(', '))
-       return msg.channel.send(emb.setTitle(`Erfolreich entfernt wurden ${kicked.lenght}`))}
+        emb.setFooter(`${kicked.length} Nutzer wurden gekickt`);
 
+        if (not_kicked.length > 0) {
+            emb.addField(`**${not_kicked.length} Nutzer wurden nicht gekickt**`,not_kicked.join(', '));
+        }
 
+        if (kicked.length > 0) {
+            emb.addField(`**Erfolreich entfernt wurden ${kicked.lenght}**`,kicked.join(', '));
+        }
 
+        msg.channel.send(emb);
     }
 
 };

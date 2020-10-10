25a21dcabae527ba213d4f22f5b892466847fad9
Kiara <frida.shivaa@gmail.com>
2020-10-10 17:38:26 +0200

    fixxing apeafixxing apearence stuffrence stuff

--- commands/2💰  Economy  💰/equip.js
+++ commands/2💰  Economy  💰/equip.js
@@ -1,13 +1,13 @@
-const {Message} = require('discord.js');
-const {rawEmb, emotes } = require('../utilities');
+const { Message } = require('discord.js');
+const { rawEmb, emotes } = require('../utilities');
 
 module.exports = {
     name: 'equip',
     syntax: 'equip <item>',
-    args: false,
+    args: true,
     description: 'Lässt dich Items ausrüsten',
     commands: ['equip'],
-    
+
     /**
      *@document
      * @this
@@ -23,45 +23,43 @@ module.exports = {
         var order = await msg.client.database.order_cache.getInventory(user.id)
         let arr = [];
 
-        for( let IID of order) {
+        for (let IID of order) {
             var item = await msg.client.database.item_cache.getConfig(IID.IID);
-           if(msg.content.toLowerCase().includes(item.NAME.toLowerCase()))
-            arr.push(item)
-         }
+            if (msg.content.toLowerCase().includes(item.NAME.toLowerCase()))
+                arr.push(item)
+        }
 
-         item = arr[0];
+        item = arr[0];
 
         if (arr.length < 1) return msg.channel.send(emb.setTitle("Dieses Item konnte nicht gefunden werden qwq"))
 
-        if(item.TYPE !== "SWORD" && item.TYPE !== "SHIELD") return msg.channel.send(emb.setTitle("Du kannst nur Schilder oder Schwerter ausrüsten"))
+        if (item.TYPE !== "SWORD" && item.TYPE !== "SHIELD") return msg.channel.send(emb.setTitle("Du kannst nur Schilder oder Schwerter ausrüsten"))
 
-        if(item.TYPE == "SWORD") {
-        
-        var old = await msg.client.database.item_cache.getConfig(player.WEAPON);
-        if(!old) {player.WEAPON ="0";}
-        else {emb.addField("Voherige Waffe", (old.NAME+ " [" + old.ATK + "/" + old.DEV + "]"))}
-        
+        if (item.TYPE == "SWORD") {
 
-        player.WEAPON = item.IID;
-        var item = await msg.client.database.item_cache.getConfig(player.WEAPON);
-        emb.addField("**Jetzige Waffe:**", (item.NAME+ " [" + item.ATK + "/" + item.DEV + "]"))
-        return player.save().then(() => msg.channel.send(emb));
-    
-        }else if(item.TYPE == "SHIELD") {
-        
-        var old = await msg.client.database.item_cache.getConfig(player.SHIELD);
-        if(!old) {player.SHIELD ="0";}
-        else {emb.addField("Voheriges Schild", (old.NAME+ " [" + old.ATK + "/" + old.DEV + "]"))}
+            var old = await msg.client.database.item_cache.getConfig(player.WEAPON);
+            if (!old) { player.WEAPON = "0"; } else { emb.addField("Voherige Waffe", (old.NAME + " [" + old.ATK + "/" + old.DEV + "]")) }
 
-        player.SHIELD = item.IID;
-        var item = await msg.client.database.item_cache.getConfig(player.SHIELD);
-        emb.addField("**Jetziges Schild:**", (item.NAME+ " [" + item.ATK + "/" + item.DEV + "]"))
-        return player.save().then(() => msg.channel.send(emb));
-        }else {return msg.channel.send(emb.setTitle("Ein Fehler ist aufgetreten"))}
-       
-       return msg.channel.send(emb)
-    
-     
+
+            player.WEAPON = item.IID;
+            var item = await msg.client.database.item_cache.getConfig(player.WEAPON);
+            emb.addField("**Jetzige Waffe:**", (item.NAME + " [" + item.ATK + "/" + item.DEV + "]"))
+            return player.save().then(() => msg.channel.send(emb));
+
+        } else if (item.TYPE == "SHIELD") {
+
+            var old = await msg.client.database.item_cache.getConfig(player.SHIELD);
+            if (!old) { player.SHIELD = "0"; } else { emb.addField("Voheriges Schild", (old.NAME + " [" + old.ATK + "/" + old.DEV + "]")) }
+
+            player.SHIELD = item.IID;
+            var item = await msg.client.database.item_cache.getConfig(player.SHIELD);
+            emb.addField("**Jetziges Schild:**", (item.NAME + " [" + item.ATK + "/" + item.DEV + "]"))
+            return player.save().then(() => msg.channel.send(emb));
+        } else { return msg.channel.send(emb.setTitle("Ein Fehler ist aufgetreten")) }
+
+        return msg.channel.send(emb)
+
+
 
     }
 };

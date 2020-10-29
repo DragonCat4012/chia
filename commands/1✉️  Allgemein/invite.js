fd915ba761fb78624ce60eaf27fba8c46e56efc9
Kiara <frida.shivaa@gmail.com>
2020-10-29 22:32:58 +0100

    fixing fight adding dungeons

---
+++ commands/1✉️  Allgemein/invite.js
@@ -0,0 +1,28 @@
+const { Message } = require('discord.js');
+const { rawEmb } = require('../utilities');
+
+module.exports = {
+    name: 'invite',
+    syntax: 'invite',
+    args: false,
+    description: 'Gibt den Linki mit dem du mich zu deinem server hinzufügen kannst UwU',
+    commands: ['invite', 'link'],
+
+    /**
+     *@document
+     * @this
+     * @param {Message} msg Nachricht in dem der Befehl geschickt wurde
+     * @param {String[]} args Argumente die im Befehl mitgeliefert wurden
+     */
+    async execute(msg) {
+        let link = "https://discord.com/oauth2/authorize?client_id=" + msg.client.user.id + "&scope=bot&permissions=289856"
+        neko = "https://discord.gg/2AGVgNM"
+        invite = "https://discord.gg/KJjZnxZ";
+        let emb = rawEmb(msg)
+            .setTitle("Invite Links")
+            .addField("**Bot-Invite**", `[Klick](${link})`)
+            .addField("**Support Server**", `[Klick](${invite})`)
+            .addField("**Neko Dev Hood**", `[Klick](${neko})`)
+        msg.channel.send(emb);
+    }
+};

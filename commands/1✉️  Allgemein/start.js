40600d733fe19afcb5ac325c439e6d2a2c62b73e
Kiara <frida.shivaa@gmail.com>
2020-12-26 11:40:08 +0100

    Update start.js

--- commands/1✉️  Allgemein/start.js
+++ commands/1✉️  Allgemein/start.js
@@ -1,5 +1,5 @@
 const { Message } = require('discord.js');
-const { rawEmb, emotes, calcLevel } = require('../utilities');
+const { rawEmb, emotes, calcLevel, colors } = require('../utilities');
 
 module.exports = {
     name: 'start',
@@ -18,6 +18,12 @@ module.exports = {
     async execute(msg) {
         let user = msg.author;
         let emb = rawEmb(msg)
+
+        let alreadyCreated = await msg.client.database.player_cache.getConfig(user.id)
+        if (alreadyCreated) {
+            emb.setDescription('**Du hast bereits ein Profil O.o**')
+            return msg.channel.send(emb.setColor(colors.error))
+        }
         var player = await msg.client.database.player_cache.addProfile(user.id);
 
         emb.setTitle(`${msg.author.username}`).setDescription('**startete sein Abenteuer, wünscht ihm/ihr/es Glück!**')

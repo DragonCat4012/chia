027f4e560e4df31a22b754cf3ed48d97bd3c6b43
Kiara <frida.shivaa@gmail.com>
2021-02-20 11:56:22 +0100

    Adding catch

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const { Message, Collection, Client, MessageEmbed } = require("discord.js");
 
 const GuildConfigShema = require('./database/GuildShema')
 const UserConfigShema = require('./database/UserShema')
+const activities_list = require('./activities.json')
 
 const { colors, newEmb, rawEmb, calcLevel, emotes } = require("./commands/utilities");
 const config = require("./config.json");
-var { token, owner } = config;
+var { token, defaultPrefix } = config;
 
 const client = new Client();
 client.config = config;
 const cooldowns = new Collection();
 var report_channel;
 
+//==================================================================================================================================================
+//      Refill Stamina
+//==================================================================================================================================================
 var now = new Date(),
     planned = new Date();
 
@@ -33,7 +37,6 @@ setTimeout(() => {
 process.on("warning", console.warn);
 
 mongoose.connect('mongodb://localhost:27017/chia?gssapiServiceName=mongodb', {
-
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -44,21 +47,19 @@ mongoose.connect('mongodb://localhost:27017/chia?gssapiServiceName=mongodb', {
     console.log("Unable to connect to the Mongodb database. Error:" + err, "error");
 });
 
-const initDatabase = async() => {
+const initDatabase = async () => {
     try {
-        for (let entr of(await GuildConfigShema.find({}))) client.database.GuildConfigCache.set(entr.guildID, entr);
-        for (let entr of(await UserConfigShema.find({}))) client.database.UserConfigCache.set(entr.userID, entr);
+        for (let entr of (await GuildConfigShema.find({}))) client.database.GuildConfigCache.set(entr.guildID, entr);
+        for (let entr of (await UserConfigShema.find({}))) client.database.UserConfigCache.set(entr.userID, entr);
         console.log(" > 🗸 Cached Database Entries");
     } catch (e) {
         console.log(" > ❌ Error While Caching Database", e)
     }
 }
-
 //==================================================================================================================================================
 //Initialize the Commands
 //==================================================================================================================================================
 client.commands = new Collection();
-
 const commandDirectorys = fs
     .readdirSync("./commands").map(name => "./commands/" + name).filter(path => fs.lstatSync(path).isDirectory());
 
@@ -76,7 +77,7 @@ for (const dir of commandDirectorys) {
 }
 //Starting the Bot
 //==================================================================================================================================================
-const start = async() => {
+const start = async () => {
     try {
         console.log("Logging in...");
         await client.login(token).catch(e => {
@@ -97,25 +98,15 @@ const start = async() => {
     }
 }
 start();
-
 //==================================================================================================================================================
-//Ready
+//Client on Ready
 //==================================================================================================================================================
-client.on("ready", async() => {
+client.on("ready", async () => {
     console.log(" >  Logged in as: " + client.user.tag);
-    client.user.setStatus("idle");
-
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
-        client.user.setActivity(activities_list[index]);
+        client.user.setActivity((activities_list[index]).replace(/{PREFIX}/i, config.defaultPrefix));
     }, 60000);
-
-    const activities_list = [
-        "Monster suchen",
-        "Shop aufräumen",
-        "Dungeons ordnen . . .",
-        `${config.prefix}help`
-    ];
 
     report_channel = client.guilds.cache
         .find(g => g.id == 553942677117337600)
@@ -127,63 +118,59 @@ client.on("ready", async() => {
 //Guild Added
 //==================================================================================================================================================
 client.on("guildCreate", async guild => {
-        let emb = rawEmb().setTitle("New Guild Joined").setDescription(`\`${guild.memberCount}\`: **${guild.name}** [${guild.id}]`)
-        report_channel.send(emb.setColor(colors.success))
+    let emb = rawEmb().setTitle("New Guild Joined").setDescription(`\`${guild.memberCount}\`: **${guild.name}** [${guild.id}]`)
+    report_channel.send(emb.setColor(colors.success))
 
-        let defaultChannel = "";
-        guild.channels.cache.forEach((channel) => {
-            if (channel.type == "text" && defaultChannel == "") {
-                if (channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
-                    defaultChannel = channel;
-                }
+    let defaultChannel = "";
+    guild.channels.cache.forEach((channel) => {
+        if (channel.type == "text" && defaultChannel == "") {
+            if (channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+                defaultChannel = channel;
             }
-        })
-        emb.setTitle("Hii mein Name lautet Chia").setColor(colors.red)
-        defaultChannel.send(emb.setDescription("**Danke das du mich auf deinen Server geholt hast** \n Nutze -help um meine Befehle zu sehen \n\nIch bin noch in der Testphase wodurch Fehler entstehen könnten qwq"))
+        }
     })
-    //==================================================================================================================================================
-    //Guild Removed
-    //==================================================================================================================================================
+    emb.setTitle("Hii mein Name lautet Chia").setColor(colors.red)
+    defaultChannel.send(emb.setDescription("**Danke das du mich auf deinen Server geholt hast** \n Nutze -help um meine Befehle zu sehen \n\nIch bin noch in der Testphase wodurch Fehler entstehen könnten qwq"))
+})
+//==================================================================================================================================================
+//Guild Removed
+//==================================================================================================================================================
 client.on("guildDelete", async guild => {
-        let emb = rawEmb().setTitle("Removed Guild").setDescription(`\`${guild.memberCount}\`: **${guild.name}** [${guild.id}]`)
-        report_channel.send(emb.setColor(colors.error))
-    })
-    //Message
-    //==================================================================================================================================================
+    let emb = rawEmb().setTitle("Removed Guild").setDescription(`\`${guild.memberCount}\`: **${guild.name}** [${guild.id}]`)
+    report_channel.send(emb.setColor(colors.error))
+})
+//Message
+//==================================================================================================================================================
 client.on("message", async message => {
-    if (message.author.bot) return;
-    if (message.channel.type == 'dm') return
-
+    if (message.author.bot || message.channel.type == 'dm') return;
     var emb = rawEmb()
     let settings = await client.database.GuildConfigCache.getConfig(message.guild.id)
-    let prefix = settings.prefix
-
+    let { prefix, levelMessage } = settings
+    //==================================================================================================================================================
     //Levelsystem
     //==================================================================================================================================================
     let CachedPlayer = await client.database.UserConfigCache.getConfig(message.author.id)
-    let old = CachedPlayer.xp
+    let oldLevel = CachedPlayer.xp
     let addedXp = Math.floor(Math.floor(Math.random() * (5 - 1 + 1) + 1))
     CachedPlayer.xp += addedXp
     await CachedPlayer.save()
-    if (settings.levelMessage) {
-        setTimeout(async() => {
-            let neu = CachedPlayer.xp;
+    if (levelMessage) {
+        let newLevel = CachedPlayer.xp;
 
-            if (calcLevel(neu) > calcLevel(old)) {
-                let channel = message.channel;
-                if (!channel) return;
-                me = message.member.toString() + " ist nun ein Level höher " + calcLevel(neu)
-                let emb = new MessageEmbed().setTitle(message.guild.name).setDescription(me).setColor(colors.nothing).setFooter(message.member.displayName).setTimestamp()
-                channel.send(emb).catch(console.log)
-            }
-        }, 1000); //Waiting for Database sync
+        if (calcLevel(newLevel) > calcLevel(oldLevel)) {
+            let channel = message.channel;
+            if (!channel) return;
+            me = message.member.toString() + " ist nun ein Level höher " + calcLevel(newLevel)
+            let emb = rawEmb().setTitle(message.guild.name).setDescription(me).setFooter(message.member.displayName).setTimestamp()
+            channel.send(emb).catch()
+        }
     }
 
     //==================================================================================================================================================
     let mentionedFirst = message.mentions.members.first()
     if (mentionedFirst) {
         if (mentionedFirst.id == client.user.id && !message.content.startsWith(prefix)) {
-            message.channel.send("Mein Prefix ist \`" + prefix + "\`")
+            message.channel.send(emb.setDescription("Mein Prefix ist \`" + prefix + "\`")).catch()
         }
     }
     if (!message.content.startsWith(prefix)) return;
@@ -194,25 +181,23 @@ client.on("message", async message => {
     if (!commandObj) return;
     const { command, module } = commandObj;
 
-    var emb = rawEmb();
     if (command.needed) {
         if (!(message.guild.me.hasPermission(command.needed))) {
             emb.setDescription("⚠️ **Mir fehlt folgende Berechtigung:** `" + command.needed).setColor(colors.error);
-            return message.channel.send(emb);
+            return message.channel.send(emb).catch()
         }
     }
 
-    var emb = rawEmb();
     if (command.perm) {
         if (command.perm == 'DEVELOPER') {
             if (!config.owner.includes(message.author.id)) {
                 emb.setDescription("⚠️ **Du bist leider kein `Developer` qwq** ").setColor(colors.error);
-                return message.channel.send(emb);
+                return message.channel.send(emb).catch()
             }
         } else {
             if (!(message.member.hasPermission(command.perm))) {
                 emb.setDescription("⚠️ **Dir fehltfolgende Berechtigung:,** `" + command.perm).setColor(colors.error);
-                return message.channel.send(emb);
+                return message.channel.send(emb).catch()
             }
         }
     }
@@ -223,7 +208,7 @@ client.on("message", async message => {
         if (command.syntax) {
             emb.addField(`Syntax`, `\`${prefix}${command.syntax}\``)
         }
-        return message.channel.send(emb);
+        return message.channel.send(emb).catch()
     }
 
     if (!cooldowns.has(command.name)) {
@@ -241,7 +226,7 @@ client.on("message", async message => {
             const timeLeft = (expirationTime - now) / 1000;
             emb
                 .addField("Cooldown", timeLeft.toFixed(1));
-            return message.channel.send(emb);
+            return message.channel.send(emb).catch()
         }
     }
 
@@ -249,10 +234,10 @@ client.on("message", async message => {
     setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
     if (command.type !== 'ALLGEMEIN' && command.type !== 'EINSTELLUNGEN') {
-        let B = await client.database.UserConfigCache.getConfig(message.author.id)
-        if (!B) {
-            let emb = rawEmb(message).setColor(colors.error).setDescription(`${message.author}**Du musst** \`-start\` **eingeben um dein Abenteuer mit mir zu starten ;-;**`)
-            return message.channel.send(emb)
+        let databaseCheck = await client.database.UserConfigCache.getConfig(message.author.id)
+        if (!databaseCheck) {
+            let emb = rawEmb().setColor(colors.error).setDescription(`${message.author}**Du musst** \`-start\` **eingeben um dein Abenteuer mit mir zu starten ;-;**`)
+            return message.channel.send(emb).catch()
         }
     }
 
@@ -263,7 +248,7 @@ client.on("message", async message => {
         emb.setDescription(
             `⚠️ Es gibt wohl noch etwas Technische Probleme mit diesem Befehl :0`
         );
-        message.channel.send(emb)
+        message.channel.send(emb).catch()
     }
 });
 
